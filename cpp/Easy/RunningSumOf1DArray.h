292ea8bee4756c8b82aa854ebaa399956984e652
Ahmed Sheta <61049695+Shetta@users.noreply.github.com>
2022-09-26 23:07:14 +0200

    Create RunningSumOf1DArray.h

---
+++ cpp/Easy/RunningSumOf1DArray.h
@@ -0,0 +1,31 @@
+
+//
+// Created by Sheta on 9/26/2022.
+// SOLVED
+//
+
+/*
+ * 1480. Running Sum of 1d Array
+ *Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+  Return the running sum of nums.
+ */
+
+#ifndef LEETCODE_FIRSTLETTERTOAPPEARTWICE_H
+#define LEETCODE_FIRSTLETTERTOAPPEARTWICE_H
+
+vector<int> runningSum(vector<int>& nums) {
+        //Initializing a vector array.
+        std::vector<int> v{};
+        //Initializing an incrementor.
+        int n = 0;
+        // Looping through the vector ref. array.
+        for(int i = 1; i<nums.size();++i){
+            // Getting the index and pushing into our new vector
+            n = n + i;
+            v.push_back(n);
+        }
+        // Returning the final result
+        return v;
+}
+
+#endif //LEETCODE_FIRSTLETTERTOAPPEARTWICE_H
