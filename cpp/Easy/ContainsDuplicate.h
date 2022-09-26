5127497f523b3f389fadbd48e7a2a25a2687ce25
Ahmed Sheta <61049695+Shetta@users.noreply.github.com>
2022-09-26 23:24:27 +0200

    Create ContainsDuplicate.h

---
+++ cpp/Easy/ContainsDuplicate.h
@@ -0,0 +1,29 @@
+//
+// Created by Sheta on 9/26/2022.
+// SOLVED
+//
+
+/*
+ * 217. Contains Duplicate
+ *Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+ */
+
+#ifndef LEETCODE_FIRSTLETTERTOAPPEARTWICE_H
+#define LEETCODE_FIRSTLETTERTOAPPEARTWICE_H
+
+bool containsDuplicate(vector<int>& nums) {
+        // Create an unordered set (O(N) Time).
+        unordered_set<int> hash_set;
+        // Traverse all the elements through the loop.
+        for(int i = 0; i < nums.size(); ++i) {
+            // Searches set. if present, it contains duplicate.
+            if(hash_set.count(nums[i]))
+                return true;
+            // insert nums[i] in set...
+            hash_set.insert(nums[i]);
+        }
+        return false;
+}
+
+
+#endif //LEETCODE_FIRSTLETTERTOAPPEARTWICE_H
